b1fcf04674cb547bbdb29c69cac1a988ac87991b
ConnaaaR <connormattless@gmail.com>
2023-10-10 14:10:14 +0100

    deploy

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('bounceApp/dist'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'bounceApp', 'dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname,'bounceApp', 'bounceApp', 'dist', 'index.html'));
     });
 }
 
