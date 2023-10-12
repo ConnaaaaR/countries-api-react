791e5ba5e1d64e732bff6fc27003c85c7af2754a
ConnaaaR <connormattless@gmail.com>
2023-10-12 21:22:42 +0100

    css

---
+++ frontend/src/components/CountryCard.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const CountryCard = ({ data }) => {
+	return (
+		<Link to={`/country/${data.name.common}`}>
+			<div className="box">
+				<h3>{data.name.official}</h3>
+				<img src={data.flags.png} alt={`Flag of ${data.name.common}`} />
+				<p>
+					<b>Population: </b>
+					{data.population}
+				</p>
+				<p>
+					<b>Capital: </b>
+					{data.capital}
+				</p>
+			</div>
+		</Link>
+	);
+};
+
+// define the prop types and shape for the data and its children
+CountryCard.propTypes = {
+	data: PropTypes.shape({
+		name: PropTypes.shape({
+			common: PropTypes.string.isRequired,
+			official: PropTypes.string.isRequired,
+		}).isRequired,
+		flags: PropTypes.shape({
+			png: PropTypes.string.isRequired,
+		}).isRequired,
+		population: PropTypes.string.isRequired,
+		capital: PropTypes.string.isRequired,
+	}).isRequired,
+};
+
+export default CountryCard;
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+
+const Form = ({ fetchData, setCountry }) => {
+	const clickHandler = (e) => {
+		e.preventDefault();
+		fetchData();
+	};
+
+	return (
+		<>
+			<div className="form-container">
+				<form className="search-form" onSubmit={clickHandler}>
+					<div className="input-container">
+						<input
+							className="search-form-text"
+							type="text"
+							autoComplete="false"
+							name="country"
+							onChange={(e) => setCountry(e.target.value)}
+						/>
+						<img className="input-icon" src="/enter-key.svg" />
+					</div>
+					<p className="under-text">
+						<img src="/bubble-ltr.svg" alt="" />
+						&nbsp;Type a country and press enter
+					</p>
+					{/* <button type="submit">Search Countries</button> */}
+				</form>
+			</div>
+		</>
+	);
+};
+
+Form.propTypes = {
+	fetchData: PropTypes.func.isRequired,
+	setCountry: PropTypes.func.isRequired,
+};
+
+export default Form;
